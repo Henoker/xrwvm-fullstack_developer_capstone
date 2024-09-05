1b28c6f7d10cff6d89a92a29734930563c15f958
Henoker <hennybany@gmail.com>
2024-09-05 04:10:51 -0400

    Build CarModel and CarMake Django Models

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,27 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
 
 # CarModelInline class
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 1
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'car_make', 'car_type', 'year', 'engine_size', 'horsepower')
+    search_fields = ('name', 'car_make__name')
+    list_filter = ('car_type', 'year', 'car_make')
+    ordering = ('year',)
 
 # CarMakeAdmin class with CarModelInline
+class CarMakeAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+    search_fields = ('name',)
+    inlines = [CarModelInline] 
 
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,8 @@
 # Uncomment the following imports before adding the Model code
 
-# from django.db import models
-# from django.utils.timezone import now
-# from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models
+from django.utils.timezone import now
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 # Create your models here.
@@ -12,6 +12,13 @@
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
 
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
@@ -23,3 +30,29 @@
 # - Year (IntegerField) with min value 2015 and max value 2023
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'Wagon'
+    TRUCK = 'Truck'
+    COUPE = 'Coupe'
+    CONVERTIBLE = 'Convertible'
+
+    CAR_TYPE_CHOICES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        (TRUCK, 'Truck'),
+        (COUPE, 'Coupe'),
+        (CONVERTIBLE, 'Convertible'),
+    ]
+
+    car_make = models.ForeignKey('CarMake', on_delete=models.CASCADE, related_name='car_models')
+    name = models.CharField(max_length=100)
+    car_type = models.CharField(max_length=20, choices=CAR_TYPE_CHOICES)
+    year = models.IntegerField(validators=[MinValueValidator(2015), MaxValueValidator(2023)])
+    engine_size = models.DecimalField(max_digits=4, decimal_places=1, blank=True, null=True)
+    horsepower = models.PositiveIntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return self.name
