34eee7db6e2c972dd15a708a3bffd0d503d097ed
Henoker <hennybany@gmail.com>
2024-09-08 08:12:27 -0400

    Fix microservices issues

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -90,15 +90,14 @@ def registration(request):
         return JsonResponse(data)
 
 
-# # Update the `get_dealerships` view to render the index page with
-# a list of dealerships
+#Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
 def get_dealerships(request, state="All"):
     if(state == "All"):
         endpoint = "/fetchDealers"
     else:
         endpoint = "/fetchDealers/"+state
     dealerships = get_request(endpoint)
-    return JsonResponse({"status":200,"dealer":dealerships})
+    return JsonResponse({"status":200,"dealers":dealerships})
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 def get_dealer_reviews(request, dealer_id):
